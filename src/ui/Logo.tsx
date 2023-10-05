49e522e0ccc05ff297627e490a613e75c9942e2d
Bogdan <thegarrybit@gmail.com>
2023-10-05 17:19:46 +0300

    feat: implement sidebar navigation

--- src/ui/Logo.tsx
+++ src/ui/Logo.tsx
@@ -1,5 +1,7 @@
 import styled from 'styled-components';
 
+import logo from '../../public/logo-light.png';
+
 const StyledLogo = styled.div`
   text-align: center;
 `;
@@ -12,7 +14,7 @@ const Img = styled.img`
 function Logo() {
   return (
     <StyledLogo>
-      <Img src="/logo-light.png" alt="Logo" />
+      <Img src={logo} alt="Logo" />
     </StyledLogo>
   );
 }
