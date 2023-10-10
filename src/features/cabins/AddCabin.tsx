28a41870a00fd51a279bfe04ecdf61d12e243e7b
Bogdan <thegarrybit@gmail.com>
2023-10-10 16:13:18 +0300

    refactor: remove unused code

--- src/features/cabins/AddCabin.tsx
+++ src/features/cabins/AddCabin.tsx
@@ -1,42 +1,19 @@
-// function AddCabin() {
-//   const [isOpenModal, setIsOpenModal] = useState(false);
-//
-//   return (
-//     <div>
-//       <Button onClick={() => setIsOpenModal((show) => !show)}>
-//         Add new cabin
-//       </Button>
-//       {isOpenModal && (
-//         <Modal onClose={() => setIsOpenModal(false)}>
-//           <CreateCabinForm onCloseModal={() => setIsOpenModal(false)} />
-//         </Modal>
-//       )}
-//     </div>
-//   );
-// }
-
-import CabinTable from './CabinTable.tsx';
 import CreateCabinForm from './CreateCabinForm.tsx';
 import Button from '../../ui/Button.tsx';
 import Modal from '../../ui/Modal.tsx';
 
 function AddCabin() {
   return (
-    <Modal>
-      <Modal.Open opens="cabin-form">
-        <Button>Add new Cabin</Button>
-      </Modal.Open>
-      <Modal.Window name="cabin-form">
-        <CreateCabinForm />
-      </Modal.Window>
-
-      <Modal.Open opens="table">
-        <Button>Show table</Button>
-      </Modal.Open>
-      <Modal.Window name="table">
-        <CabinTable />
-      </Modal.Window>
-    </Modal>
+    <div>
+      <Modal>
+        <Modal.Open opens="cabin-form">
+          <Button>Add new Cabin</Button>
+        </Modal.Open>
+        <Modal.Window name="cabin-form">
+          <CreateCabinForm />
+        </Modal.Window>
+      </Modal>
+    </div>
   );
 }
 
