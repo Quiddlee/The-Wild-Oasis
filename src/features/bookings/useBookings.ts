65d22b06241496b03fcdda624e3b80ee74c1ee2d
Bogdan <thegarrybit@gmail.com>
2023-10-15 22:15:57 +0300

    feat: implement pagination pages pre-fetch

--- src/features/bookings/useBookings.ts
+++ src/features/bookings/useBookings.ts
@@ -1,12 +1,18 @@
-import { useQuery } from '@tanstack/react-query';
+import { useQuery, useQueryClient } from '@tanstack/react-query';
 
 import useUrl from '../../hooks/useUrl.ts';
 import getBookings from '../../services/apiBookings.ts';
 import { BookingFilterValues, BookingSortValues } from '../../types/enums.ts';
 import { BookingsFilterValueTypes } from '../../types/types.ts';
-import { QUERY_PAGE, QUERY_SORT, QUERY_STATUS } from '../../utils/const.ts';
+import {
+  MAX_ITEMS_ON_PAGE,
+  QUERY_PAGE,
+  QUERY_SORT,
+  QUERY_STATUS,
+} from '../../utils/const.ts';
 
 function useBookings() {
+  const queryClient = useQueryClient();
   const { readUrl } = useUrl();
 
   // Filter
@@ -28,11 +34,30 @@ function useBookings() {
   // Pagination
   const pageQuery = readUrl(QUERY_PAGE);
   const page = !pageQuery ? 1 : Number(pageQuery);
+  const prevPage = page - 1;
+  const nextPage = page + 1;
 
+  // Query
   const { data: { data: bookings, count } = {}, isLoading } = useQuery({
     queryKey: ['bookings', filter, sortBy, page],
     queryFn: () => getBookings(filter, sortBy, page),
   });
+
+  // Pre-fetching next page
+  const pageCount = Math.ceil((count ?? 0) / MAX_ITEMS_ON_PAGE);
+
+  if (page < pageCount)
+    void queryClient.prefetchQuery({
+      queryKey: ['bookings', filter, sortBy, nextPage],
+      queryFn: () => getBookings(filter, sortBy, nextPage),
+    });
+
+  // Pre-fetching prev page
+  if (page > 1)
+    void queryClient.prefetchQuery({
+      queryKey: ['bookings', filter, sortBy, prevPage],
+      queryFn: () => getBookings(filter, sortBy, prevPage),
+    });
 
   return { bookings, isLoading, count };
 }
