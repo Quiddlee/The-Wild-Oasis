5059a549967928cd6dd7d6815db9f913228caf12
Bogdan <bogdanscherbinadev@gmail.com>
2023-11-29 23:15:05 +0200

    feat: implement header

--- src/features/authentication/UserAvatar.tsx
+++ src/features/authentication/UserAvatar.tsx
@@ -1,4 +1,7 @@
-/*
+import styled from 'styled-components';
+
+import useUser from './useUser.ts';
+
 const StyledUserAvatar = styled.div`
   display: flex;
   gap: 1.2rem;
@@ -10,7 +13,6 @@ const StyledUserAvatar = styled.div`
 
 const Avatar = styled.img`
   display: block;
-  width: 4rem;
   width: 3.6rem;
   aspect-ratio: 1;
   object-fit: cover;
@@ -19,4 +21,22 @@ const Avatar = styled.img`
   outline: 2px solid var(--color-grey-100);
 `;
 
- */
+function UserAvatar() {
+  const { user } = useUser();
+
+  if (!user) return null;
+
+  const { fullName, avatar } = user.user_metadata;
+
+  return (
+    <StyledUserAvatar>
+      <Avatar
+        src={avatar || 'default-user.jpg'}
+        alt={`Avatar of ${fullName}`}
+      />
+      <span>{fullName}</span>
+    </StyledUserAvatar>
+  );
+}
+
+export default UserAvatar;
