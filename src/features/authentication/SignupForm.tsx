60facc1d11ee7089d1e4d403286e4b713a423bbe
Bogdan <bogdanscherbinadev@gmail.com>
2023-11-29 22:26:14 +0200

    feat: add sign up form

--- src/features/authentication/SignupForm.tsx
+++ src/features/authentication/SignupForm.tsx
@@ -1,32 +1,83 @@
-// Email regex: /\S+@\S+\.\S+/
+import { useForm } from 'react-hook-form';
+
+import Button from '../../ui/Button.tsx';
+import Form from '../../ui/Form.tsx';
+import FormRow from '../../ui/FormRow.tsx';
+import Input from '../../ui/Input.tsx';
 
 function SignupForm() {
-  return <div />;
-  // <Form>
-  //   <FormRow label="Full name" error="">
-  //     <Input type="text" id="fullName" />
-  //   </FormRow>
-  //
-  //   <FormRow label="Email address" error="">
-  //     <Input type="email" id="email" />
-  //   </FormRow>
-  //
-  //   <FormRow label="Password (min 8 characters)" error="">
-  //     <Input type="password" id="password" />
-  //   </FormRow>
-  //
-  //   <FormRow label="Repeat password" error="">
-  //     <Input type="password" id="passwordConfirm" />
-  //   </FormRow>
-  //
-  //   <FormRow>
-  //     {/* type is an HTML attribute! */}
-  //     <Button variation="secondary" type="reset">
-  //       Cancel
-  //     </Button>
-  //     <Button>Create new user</Button>
-  //   </FormRow>
-  // </Form>
+  const { register, formState, handleSubmit } = useForm();
+  const { errors } = formState;
+
+  function onSubmit(/* data: FieldValues */) {
+    // console.log(data);
+  }
+
+  return (
+    <Form onSubmit={handleSubmit(onSubmit)}>
+      <FormRow label="Full name" error={errors?.fullName?.message as string}>
+        <Input
+          type="text"
+          id="fullName"
+          {...register('fullName', { required: 'This field is required' })}
+        />
+      </FormRow>
+
+      <FormRow label="Email address" error={errors?.email?.message as string}>
+        <Input
+          type="email"
+          id="email"
+          {...register('email', {
+            required: 'This field is required',
+            pattern: {
+              value: /\S+@\S+\.\S+/,
+              message: 'Provide a valid email address',
+            },
+          })}
+        />
+      </FormRow>
+
+      <FormRow
+        label="Password (min 8 characters)"
+        error={errors?.password?.message as string}>
+        <Input
+          type="password"
+          id="password"
+          {...register('password', {
+            required: 'This field is required',
+            minLength: {
+              value: 8,
+              message: 'Password need a minimum of 8 characters',
+            },
+          })}
+        />
+      </FormRow>
+
+      <FormRow
+        label="Repeat password"
+        error={errors?.passwordConfirm?.message as string}>
+        <Input
+          type="password"
+          id="passwordConfirm"
+          {...register('passwordConfirm', {
+            required: 'This field is required',
+            validate: (value, formValues) =>
+              value === formValues.password || 'Password need to match',
+          })}
+        />
+      </FormRow>
+
+      <FormRow>
+        <>
+          {/* type is an HTML attribute! */}
+          <Button variation="secondary" type="reset">
+            Cancel
+          </Button>
+          <Button>Create new user</Button>
+        </>
+      </FormRow>
+    </Form>
+  );
 }
 
 export default SignupForm;
--- src/pages/Users.tsx
+++ src/pages/Users.tsx
@@ -1,7 +1,13 @@
+import SignupForm from '../features/authentication/SignupForm.tsx';
 import Heading from '../ui/Heading.tsx';
 
 function NewUsers() {
-  return <Heading as="h1">Create a new user</Heading>;
+  return (
+    <>
+      <Heading as="h1">Create a new user</Heading>
+      <SignupForm />
+    </>
+  );
 }
 
 export default NewUsers;
